7b274723465857690772f9b829604faae30d4836
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-06 09:54:39 +0700

    fix post schema

--- models/Posts.model.js
+++ models/Posts.model.js
@@ -27,7 +27,8 @@ const Post = mongoose.model(
 			},
 			file: {
 				type: String,
-				default: '',
+				default:
+					'https://res.cloudinary.com/dlvk5v5jr/image/upload/v1656904678/noimage_food_viet247_r3nlzm.jpg',
 			},
 			userSaved: {
 				type: [String],
