c58bf2f718b5ec3eeb332d9e1a8eaeca208f4d68
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-07 09:14:16 +0700

    update addComment

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -157,7 +157,9 @@ exports.updatePost = async (req, res) => {
 					},
 				},
 				{ new: true }
-			).lean();
+			)
+				.populate([{ path: 'user', select: '_id username profilePicture' }])
+				.lean();
 			if (post) {
 				return res.status(200).json(post);
 			} else {
