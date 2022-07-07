7c50c7c9a4b7e1442e74a24bde91285a34014291
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-07 22:58:14 +0700

    fix ...

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -16,7 +16,7 @@ exports.getAllPosts = async (req, res) => {
 
 		// cac post co author trong followings
 		const posts = await Post.find({ author: { $in: followings } })
-			.sort({ createdAt: 'asc' }) // sap xep asc
+			.sort({ createdAt: 'desc' }) // sap xep desc
 			.skip(perPage * page - perPage)
 			.limit(perPage)
 			.populate([
@@ -42,7 +42,7 @@ exports.getAllPostsByUser = async (req, res) => {
 	try {
 		// cac post co author trong followings
 		const posts = await Post.find({ author: new mongoose.Types.ObjectId(authorId) })
-			.sort({ createdAt: 'asc' }) // sap xep asc
+			.sort({ createdAt: 'desc' }) // sap xep desc
 			.skip(perPage * page - perPage)
 			.limit(perPage)
 			.populate([
