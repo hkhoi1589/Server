aa33fe626c4210cec6e90a2aef14fcdb61de8ccc
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-09 17:44:15 +0700

    fix...

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ app.use(morgan('common'));
 
 // Socket
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+	cors: {
+		origin: 'http://localhost:3000',
+	},
+});
 io.on('connection', (socket) => {
 	SocketServer(socket);
 });
