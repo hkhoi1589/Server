d9e37cfdcbe2bade370097ab42bac6d27c0d0381
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-10 09:15:28 +0700

    fix createNotify

--- socketServer.js
+++ socketServer.js
@@ -65,7 +65,7 @@ const SocketServer = (socket) => {
 	// Notification
 	socket.on('createNotify', (msg) => {
 		const client = users.find((user) => user.id === msg.clientId);
-		client && socket.to(`${client.socketId}`).emit('createNotifyToClient', msg); // gui lai client bi follow
+		client && socket.to(`${client.socketId}`).emit('createNotifyToClient', msg);
 	});
 
 	socket.on('removeNotify', (msg) => {
