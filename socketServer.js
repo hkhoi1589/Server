8e12324d80e5aee7c3f6d43815ce4168d039294d
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-11 09:36:22 +0700

    f

--- socketServer.js
+++ socketServer.js
@@ -97,10 +97,8 @@ const SocketServer = (socket, io) => {
 		const following = users.filter((user) =>
 			data.following.some((item) => item._id === user.id)
 		);
-		console.log('Data', data);
-		console.log('Following', following);
 		// tra ve following cho user
-		socket.emit('checkUserOnlineToMe', following.id);
+		socket.emit('checkUserOnlineToMe', following);
 
 		// tim followers dang online
 		const clients = users.filter((user) => data.followers.some((item) => item._id === user.id));
