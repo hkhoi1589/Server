286d19568d825285b31f6284808d60a3abd006bf
hkhoi1589 <hkhoi1589@gmail.com>
2022-07-12 12:11:09 +0700

    f

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -304,7 +304,7 @@ exports.follow = async (req, res) => {
 			{ new: true }
 		);
 
-		return res.status(200).json({ message: 'Followed this user', type: 'success', user });
+		return res.status(200).json(user);
 	} catch (error) {
 		return res.status(500).json({ message: error.message, type: 'error' });
 	}
@@ -339,7 +339,7 @@ exports.unfollow = async (req, res) => {
 			{ new: true }
 		);
 
-		return res.status(200).json({ message: 'Unfollowed this user', type: 'success', user });
+		return res.status(200).json(user);
 	} catch (error) {
 		return res.status(500).json({ message: error.message, type: 'error' });
 	}
